523ed5b05805229f19f9ec9edefbe723463bf35c
hungvu <hungns126@gmail.com>
2025-03-14 16:14:56 +0700

    write

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 // JSON Server module
 const jsonServer = require("json-server");
+const fs = require('fs')
+const path = require('path')
 const server = jsonServer.create();
-const router = jsonServer.router("db/db.json");
+
+const filePath = path.join('db/db.json')
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db)
+
+// const router = jsonServer.router("db/db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
